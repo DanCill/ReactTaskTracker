9d31f7d1ffc12afa3d45fd067efdd8be3bcdded4
Daniel <danielc@runninghill.co.za>
2021-02-18 08:04:20 +0200

    Added delete icon and function, adding task form added

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,11 +1,47 @@
+import { useState } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+    const [tasks, setTasks] = useState(
+        [{
+            id: 1,
+            text: 'Doc App',
+            day: 'Feb 5th at 2:00 PM',
+            reminder: 'true'
+        },
+            {
+                id: 2,
+                text: 'Doc App 2.0',
+                day: 'Feb 5th at 2:00 PM',
+                reminder: 'true'
+            },
+            {
+                id: 3,
+                text: 'Food Shopping',
+                day: 'Feb 18th at 12:00 PM',
+                reminder: 'true'
+            }
+        ]);
+
+    const addTask = (task) => {
+        console.log(task);
+    }
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    }
+
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    }
+
      return (
     <div className="container">
         <Header />
-        <Tasks />
+        <AddTask onAdd={addTask} />
+        {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
 }
--- task-tracker/src/components/Tasks.js
+++ task-tracker/src/components/Tasks.js
@@ -1,24 +1,13 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doc App',
-        day: 'Feb 5th at 2:00 PM',
-        reminder: 'true'
-    },
-    {
-        id: 2,
-        text: 'Doc App 2.0',
-        day: 'Feb 5th at 2:00 PM',
-        reminder: 'true'
-    }
-];
+import Task from './Task';
 
-const Tasks = () => {
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
-            {
-                tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))
-            }
+            {tasks.map((task) => (
+                <Task key={task.id} task={task}
+                onDelete={onDelete} onToggle={onToggle}/>
+            ))}
         </>
     )
 }
